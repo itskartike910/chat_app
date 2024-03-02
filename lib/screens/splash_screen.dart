ff4cf0aab327970b6065f22a3633902b85936bf0
Kartik kumar <kumarkartik147359@gmail.com>
2024-03-02 15:06:48 +0530

    created splah screen and added homepage

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+class SplashScreen extends StatefulWidget {
+  final Widget? child;
+  const SplashScreen({super.key, this.child});
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+class _SplashScreenState extends State<SplashScreen> {
+@override
+  void initState() {
+    Future.delayed(
+     const Duration(seconds: 3),(){
+        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => widget.child!), (route) => false);
+    }
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Welcome To The ChatBox",
+           style: TextStyle(
+            color: Colors.blue,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
