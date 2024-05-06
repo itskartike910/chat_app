3c086346344ee557604789ad8c551f16e2c7f621
Kartik kumar <kumarkartik147359@gmail.com>
2024-05-06 22:49:25 +0530

    Added Firebase methods

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:chat_app/authentication/login_page.dart';
 import 'package:chat_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
